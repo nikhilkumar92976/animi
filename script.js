7ac25f226400552350cd28c5e3fa9d8b1e29ed38
Nikhil Kumar <158755917+nikhilkumar92976@users.noreply.github.com>
2024-08-18 00:42:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var tl = gsap.timeline()
+tl.from("h3",{
+    y:-50,
+    opacity:0,
+    delay:.4,
+    duration:0.5,
+    stagger:0.4
+})
+tl.from("h1",{
+    x:-500,
+    opacity:0,
+    duration:1,
+    stagger:0.4
+})
+tl.from("img",{
+    x:100,
+    opacity:0,
+    duration:.4,
+    stagger:0.4,
+    rotate:45
+})
